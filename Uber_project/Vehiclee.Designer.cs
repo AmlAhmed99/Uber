f5f0c6d9521d68b8c0b53526a887e2be058d483e
AmlAhmed99 <amlahmedsaid1@gmail.com>
2020-04-11 17:10:00 +0200

    update1

--- Uber_project/Vehiclee.Designer.cs
+++ Uber_project/Vehiclee.Designer.cs
@@ -40,6 +40,8 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -105,7 +107,7 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Location = new System.Drawing.Point(543, 74);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(285, 476);
+            this.panel1.Size = new System.Drawing.Size(286, 476);
             this.panel1.TabIndex = 2;
             // 
             // button4
@@ -174,6 +176,34 @@
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(369, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 37);
+            this.button1.TabIndex = 47;
+            this.button1.Text = "DeleteVehicle";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(790, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 37);
+            this.button2.TabIndex = 48;
+            this.button2.Text = "UpdateVehicle";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // Vehiclee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -181,6 +211,8 @@
             this.BackgroundImage = global::Uber_project.Properties.Resources._623ebd81950975_5d0f16eee3bba;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1354, 688);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.panel1);
             this.Name = "Vehiclee";
@@ -205,5 +237,7 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
